f76a38f063d81be18bec10a93d18fe42fb62b9cb
Danu Ramdhani <118526320+danu-ramdhani@users.noreply.github.com>
2023-08-26 20:45:36 +0800

    Resolved merge conflict by incorporating both suggestions.

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,13 +1,75 @@
-import 'package:fav_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'dart:io';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+
+import 'package:fav_places/models/place.dart';
+
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, date INTEGER)');
+    },
+    version: 1,
+  );
+
+  return db;
+}
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title, File image, DateTime date) {
-    final newPlace = Place(title: title, image: image, date: date);
+  Future<void> loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+    final places = data
+        .map(
+          (row) => Place(
+            id: row['id'] as String,
+            title: row['title'] as String,
+            image: File(row['image'] as String),
+            date: DateTime.fromMillisecondsSinceEpoch(row['date'] as int),
+          ),
+        )
+        .toList();
+
+    state = places;
+  }
+
+  void addPlace(String title, File image, DateTime date) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$filename');
+
+    final newPlace = Place(title: title, image: copiedImage, date: date);
+    final db = await _getDatabase();
+    await db.insert(
+      'user_places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path,
+        'date': newPlace.date.millisecondsSinceEpoch, // Convert to milliseconds
+      },
+    );
+
     state = [newPlace, ...state];
+  }
+
+  Future<void> deletePlace(String placeId) async {
+    final db = await _getDatabase();
+    await db.delete(
+      'user_places',
+      where: 'id = ?',
+      whereArgs: [placeId],
+    );
+
+    state = state.where((place) => place.id != placeId).toList();
   }
 }
 
--- lib/screen/places.dart
+++ lib/screen/places.dart
@@ -6,11 +6,25 @@ import 'package:fav_places/providers/user_places.dart';
 import 'package:fav_places/screen/account.dart';
 import 'package:fav_places/widgets/list_place.dart';
 
-class PlacesScreen extends ConsumerWidget {
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<PlacesScreen> createState() => _PlacesScreenState();
+}
+
+class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+late Future<void> _placesFuture;
+
+@override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
     final userPlaces = ref.watch(userPlacesProvider);
 
     return DefaultTabController(
@@ -57,6 +71,30 @@ class PlacesScreen extends ConsumerWidget {
           ],
         ),
       ),
+<<<<<<< HEAD
+=======
+      body: FutureBuilder(
+        future: _placesFuture,
+        builder: (context, snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ListPlace(userPlaces: userPlaces),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const AddPlace(),
+            ),
+          );
+        },
+        shape: const CircleBorder(),
+        child: const FaIcon(FontAwesomeIcons.plus),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+>>>>>>> 37c5772577c783bc7e13c656e1535b6cdb474596
     );
   }
 }
